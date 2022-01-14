c0a6255ab61d88b22f652dfb98dd55087100ce04
Jack Kim <kimhyjun@umich.edu>
2022-01-14 10:02:26 +0900

    still working on collections

--- Collections/src/com/jack/Main.java
+++ Collections/src/com/jack/Main.java
@@ -1,22 +1,42 @@
 package com.jack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         Theater theater = new Theater("Olympian", 8, 12);
-        //theater.getSeats();
-        if (theater.reserveSeat("H11")) {
-            System.out.println("Please Pay");
-        } else {
-            System.out.println("Sorry, seat is already taken");
-        }
+        List<Theater.Seat> seatCopy = new ArrayList<>(theater.seats);
+        printList(seatCopy);
 
-
-        if (theater.reserveSeat("H11")) {
-            System.out.println("Please Pay");
+        seatCopy.get(1).reserve();
+        if (theater.reserveSeat("A02")){
+            System.out.println("Please pay for A02");
         } else {
-            System.out.println("Sorry, seat is already taken");
+            System.out.println("Seat already reserved");
         }
+//        theater.getSeats();
+//        if (theater.reserveSeat("H11")) {
+//            System.out.println("Please Pay");
+//        } else {
+//            System.out.println("Sorry, seat is already taken");
+//        }
+//
+//
+//        if (theater.reserveSeat("H11")) {
+//            System.out.println("Please Pay");
+//        } else {
+//            System.out.println("Sorry, seat is already taken");
+//        }
     }
+
+        public static void printList(List<Theater.Seat> list) {
+            for (Theater.Seat seat : list) {
+                System.out.print(" " + seat.getSeatNumber());
+            }
+            System.out.println();
+            System.out.println("=======================================================");
+        }
 }
